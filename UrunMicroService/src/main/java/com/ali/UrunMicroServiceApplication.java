75c89a32be4789600c9b79934880738efac9983c
aliogutcen <aliogutcen@gmail.com>
2023-03-01 14:23:57 +0300

    Initial commit

---
+++ UrunMicroService/src/main/java/com/ali/UrunMicroServiceApplication.java
@@ -0,0 +1,16 @@
+package com.ali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrunMicroServiceApplication {
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(UrunMicroServiceApplication.class);
+
+
+
+    }
+}
